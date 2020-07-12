bc4f06323ec3ac23fdae670845b13ffd2003621b
Ubuntu <ubuntu@ip-172-31-35-249.ec2.internal>
2020-07-12 06:03:04 +0000

    transformer cache optimization

--- cachelab-handout/trans.c
+++ cachelab-handout/trans.c
@@ -24,10 +24,139 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
 }
 
+void printMatrix(int M, int N, int A[N][M]) {
+	int i, j;
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < M; j++) {
+			printf("%3d", A[i][j]);
+		}
+		printf("\n");
+	}
+}
+
 /* 
  * You can define additional transpose functions below. We've defined
  * a simple one below to help you get started. 
  */ 
+
+char trans_exp_desc_2[] = "transpose 32 * 32";
+void trans_exp_2(int M, int N, int A[N][M], int B[M][N])
+{
+	//trans_help(M, N, 0, 8, N-8, N, A, B);
+	//trans_help(M, N, 8, M, N-8, N, A, B);
+	//trans_help(M, N, 0, 8, 0, N-8, A, B);
+	//trans_help(M, N, 8, M, 0, N-8, A, B);
+    int i, j, k, l, dia, flag;
+	int blocksize = 8;
+
+    for (i = 0; i < N; i += blocksize) {
+        for (j = 0; j < M; j += blocksize) {
+         	for (k = i; k < i+blocksize; k++) {
+				flag = 0;
+				for (l = j; l < j+blocksize; l++) {
+					if (k == l) {
+						dia = A[k][l];
+						flag = 1;
+					} else {
+						B[l][k] = A[k][l];
+					}
+				}
+				if (flag == 1) {
+					B[k][k] = dia;
+				}
+			}	
+        }
+    }    
+}
+
+
+
+char trans_exp_desc_3[] = "transpose 64*64";
+void trans_exp_3(int M, int N, int A[N][M], int B[M][N])
+{
+    int i, j, k, l;
+	int blocksize = 4;
+	//int kk, ll;
+	//int line, lineFlag;
+	int temp[4][2];
+
+    for (i = 0; i < N; i += blocksize) {
+        for (j = 0; j < M; j += blocksize) {
+			if (i == j) {
+				temp[0][0] = A[i][j];
+				temp[0][1] = A[i][j+1];
+				temp[1][0] = A[i+1][j];
+				temp[1][1] = A[i+1][j+1];
+			
+				B[j][i+2] = A[i+2][j];
+				B[j+1][i+2] = A[i+2][j+1];
+				B[j][i+3] = A[i+3][j];
+				B[j+1][i+3] = A[i+3][j+1];
+
+				B[i][j]	= temp[0][0];
+				B[i][j+1] = temp[1][0];
+				B[i+1][j] = temp[0][1];
+				B[i+1][i+1] = temp[1][1];
+		
+				temp[0][0] = A[i+2][j+2];
+				temp[0][1] = A[i+2][j+3];
+				temp[1][0] = A[i+3][j+2];
+				temp[1][1] = A[i+3][j+3];
+
+				B[j+2][i] = A[i][j+2];
+				B[j+3][i] = A[i][j+3];
+				B[j+2][i+1] = A[i+1][j+2];
+				B[j+3][i+1] = A[i+1][j+3];
+
+				B[i+2][j+2] = temp[0][0];
+				B[i+2][j+3] = temp[1][0];
+				B[i+3][j+2] = temp[0][1];
+				B[i+3][i+3] = temp[1][1];
+				
+			} else if (((j == 4 || j == 12 || j == 20 || j == 28 || j == 36 || j == 44 || j == 52 || j == 60) && j-i==4) || ((j == 0 || j == 8 || j == 16 || j == 24 || j == 32 || j == 40 || j == 48 || j == 56) && i-j==4)) {
+
+				temp[0][0] = A[i][j+2];
+                temp[0][1] = A[i][j+3];
+                temp[1][0] = A[i+1][j+2];
+                temp[1][1] = A[i+1][j+3];
+            
+                temp[2][0] = A[i+2][j];
+                temp[2][1] = A[i+2][j+1];
+                temp[3][0] = A[i+3][j];
+                temp[3][1] = A[i+3][j+1];
+            
+                B[j][i] = A[i][j];
+                B[j+1][i] = A[i][j+1];
+                B[j][i+1] = A[i+1][j];
+                B[j+1][i+1] = A[i+1][j+1];
+            
+                B[j+2][i+2] = A[i+2][j+2];
+                B[j+3][i+2] = A[i+2][j+3];
+                B[j+2][i+3] = A[i+3][j+2];
+                B[j+3][i+3] = A[i+3][j+3];
+            
+                B[j+2][i] = temp[0][0];
+                B[j+3][i] = temp[0][1];
+                B[j+2][i+1] = temp[1][0];
+                B[j+3][i+1] = temp[1][1];
+            
+                B[j][i+2] = temp[2][0];
+                B[j+1][i+2] = temp[2][1];
+                B[j][i+3] = temp[3][0];
+                B[j+1][i+3] = temp[3][1];
+
+			} else {
+         		for (k = i; k < i+blocksize; k++) {
+					for (l = j; l < j+blocksize; l++) {
+						B[l][k] = A[k][l];
+					}
+				}	
+			}
+        }
+    }    
+
+}
+
 
 /* 
  * trans - A simple baseline transpose function, not optimized for the cache.
@@ -56,7 +185,11 @@ void trans(int M, int N, int A[N][M], int B[M][N])
 void registerFunctions()
 {
     /* Register your solution function */
-    registerTransFunction(transpose_submit, transpose_submit_desc); 
+    //registerTransFunction(transpose_submit, transpose_submit_desc); 
+
+    registerTransFunction(trans_exp_2, trans_exp_desc_2); 
+    registerTransFunction(trans_exp_3, trans_exp_desc_3); 
+    registerTransFunction(trans_exp_4, trans_exp_desc_4); 
 
     /* Register any additional transpose functions */
     registerTransFunction(trans, trans_desc); 
